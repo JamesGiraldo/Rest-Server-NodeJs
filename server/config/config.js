8a688de09da4f5f9d646ae47389de45061a59b03
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-10-12 22:33:58 -0500

    Rest-Server-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,26 @@
 // Puerto
 // ===================
 process.env.PORT = process.env.PORT || 3000;
+// ===========================
+// CREDENCIAL DE MONGO ATLAS
+// usuario: james
+// password: JPxcOmsPKkXB5IcI
+// ===========================
+// mongodb+srv://james:JPxcOmsPKkXB5IcI@cluster0.0m6ol.mongodb.net/cafe
+// ===========================
+
+// ===========================
+// ENTORNO
+// ===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===========================
+// BASE DE DATOS
+// ===========================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://james:JPxcOmsPKkXB5IcI@cluster0.0m6ol.mongodb.net/cafe'
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,44 +1,22 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const colors = require('colors');
 const app = express();
 const bodyParser = require('body-parser');
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-    // Parse application/json
+
+// Parse application/json
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-    res.json('Hello Wold');
-})
-app.get('/usuario', function(req, res) {
-    res.json('Get usuario');
-})
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+app.use(require('./routes/usuario'));
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-})
-app.delete('/usuario', function(req, res) {
-    res.json('Delete Wold');
-})
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
+    if (err) throw err;
+    console.log(`Base de Datos`.blue, `ONLINE`.green, `!!!`.red);
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto:`.yellow, `${ process.env.PORT }`.blue);
