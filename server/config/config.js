5e3d41542a75c9eb11daacc5bd0c67ebfb14f8e2
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-10-12 22:54:24 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,6 +22,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://james:JPxcOmsPKkXB5IcI@cluster0.0m6ol.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
